0f9f8bf23262e5d60bf3c2e944f1bc3af55736a2
Joe Harlow <joe@f5.io>
2013-07-12 10:21:56 +0100

    Fix for IE attachEvent scope

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -1,24 +1,26 @@
 (function() {
 
-	Satnav({ 
-		html5 : false,
-		force : true
-	})
-		.navigate({
-			path : '/?{filter}/?{page}',
-			directions : function(params) {
-				console.log('directions received');
-				console.log(params);
-			}
+	window.onload = function() {
+		Satnav({
+			html5 : false,
+			force : true
 		})
-		.change(function(params, old) {
-			console.log('change event heard');
-			setTimeout(function() {
-				Satnav.resolve();
-			}, 2000);
-			return this.defer;
-		})
-		.otherwise('/')
-		.go();
+			.navigate({
+				path : '/?{filter}/?{page}',
+				directions : function(params) {
+					console.log('directions received');
+					console.log(params);
+				}
+			})
+			.change(function(params, old) {
+				console.log('change event heard');
+				setTimeout(function() {
+					Satnav.resolve();
+				}, 2000);
+				return this.defer;
+			})
+			.otherwise('/')
+			.go();
+	};
 
 })();
